183f0348652692ba5b62aab800ace87004e4afb7
Luc Vedrenne <luc.vedrenne@imt-atlantique.net>
2020-11-14 12:26:34 +0100

    fix loss computation

--- model.py
+++ model.py
@@ -97,8 +97,10 @@ class LightningModel(pl.LightningModule):
         coeffs = [1.] + 2*[0.8, 0.7, 0.6, 0.5]
         if self.net.training and self.hparams.deep_supervision:
             for coeff, output in zip(coeffs, outputs):
+                print(type(output))
                 loss += coeff * loss_function(output, targets)
         else:
+            print(type(outputs[0]))
             loss = loss_function(outputs[0], targets)
         return loss
 
