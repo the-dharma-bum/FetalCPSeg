30b1670c493044d28813f575962d6c739e539ee1
Luc Vedrenne <luc.vedrenne@imt-atlantique.net>
2020-11-15 13:49:29 +0100

    network rewritting in progress

--- network.py
+++ network.py
@@ -96,7 +96,7 @@ class FastMixBlock(nn.Module):
 
 # +---------------------------------------------------------------------------------------------+ #
 # |                                                                                             | #
-# |                                           FAST MIX BLOCK                                    | #
+# |                                      ATTENTION (with MD Conv)                               | #
 # |                                                                                             | #
 # +---------------------------------------------------------------------------------------------+ #
 
@@ -131,59 +131,85 @@ class Attention(nn.Module):
 
 class MixAttNet(nn.Module):
     
-    def __init__(self, in_channels: int=1, attention: bool=True, num_blocks: int=4):
+    def __init__(self, in_channels: int=1, 
+                 attention: bool=True, supervision: bool=True, depth: int=4) -> None:
         super().__init__()
-        self.attention  = attention
-        self.num_blocks = num_blocks
-        dim = [16] + [2 ** (4+i) for i in range(num_blocks)] + [2 ** (4+num_blocks-1)]
-        strides = [1] + num_blocks * [2]
-        self.init_block = ConvBlock3D(in_channels, 16)
-        self.encoders   = nn.ModuleList(
-            [ResBlock(dim[i], dim[i+1], stride=strides[i]) for i in range(num_blocks+1)])
-        self.decoders   = nn.ModuleList([ResBlock(2*dim[i+1],  dim[i]) for i in range(num_blocks)])
-        self.down       = nn.ModuleList(
-            [ConvBlock3D(dim[i] , 16, kernel_size=1) for i in range(num_blocks)])
-        self.down_out   = nn.ModuleList(num_blocks * [nn.Conv3d(16, 1, kernel_size=1)])
-        if self.attention:
-            self.mix    = nn.ModuleList(num_blocks * [Attention(16, 16)])
-        self.mix_out    = nn.ModuleList(num_blocks * [nn.Conv3d(16, 1, kernel_size=1)])
-        self.last_block = ConvBlock3D(num_blocks * 16, 64, bias=True)
-        self.final_conv = nn.Conv3d(64, 1, kernel_size=1)
-        self.non_linear = nn.PReLU()
+        dim = [16] + [2 ** (4+i) for i in range(depth)] + [2 ** (4+depth-1)]
+        self.use_attention = attention
+        self.supervision   = supervision
+        self.depth         = depth
+        self.init_block    = ConvBlock3D(in_channels, 16)
+        self.encoders      = self._init_encoders(depth, dim)
+        self.decoders      = self._init_decoders(depth, dim)
+        self.down          = self._init_down_modules(depth, dim) 
+        if self.use_attention:
+            self.attention = nn.ModuleList(depth * [Attention(16, 16)])
+        self.down_out      = nn.ModuleList(depth * [nn.Conv3d(16, 1, kernel_size=1)])
+        self.mix_out       = nn.ModuleList(depth * [nn.Conv3d(16, 1, kernel_size=1)])
+        self.last_block    = ConvBlock3D(depth * 16, 64, bias=True)
+        self.final_conv    = nn.Conv3d(64, 1, kernel_size=1)
+        self.non_linear    = nn.PReLU()
 
-    def feedforward_encoders(self, x):
+    @staticmethod
+    def _init_encoders(depth, dim):
+        strides = [1] + depth * [2]
+        encoders = nn.ModuleList()
+        for i in range(depth+1):
+            encoders.append(ResBlock(dim[i], dim[i+1], stride=strides[i]))
+        return encoders
+    
+    @staticmethod
+    def _init_decoders(depth, dim):
+        return nn.ModuleList([ResBlock(2*dim[i+1],  dim[i]) for i in range(depth)])
+
+    @staticmethod
+    def _init_down_modules(depth, dim):
+        return nn.ModuleList([ConvBlock3D(dim[i] , 16, kernel_size=1) for i in range(depth)])
+
+    def through_encoders(self, x):
         encoders_outputs = [self.encoders[0](x)]
-        for i in range(self.num_blocks):
+        for i in range(self.depth):
             encoders_outputs.append(self.encoders[i+1](encoders_outputs[i]))
         return encoders_outputs
 
-    def feedforward_decoders(self, encoders_outputs):
+    def through_decoders(self, encoders_outputs):
         decoders_outputs = [self.decoders[-1](
             F.up_sample_and_concat3d(encoders_outputs[-2], encoders_outputs[-1]))]
-        for i in reversed(range(self.num_blocks-1)):
+        for i in reversed(range(self.depth-1)):
             decoders_outputs.append(self.decoders[i](
                 F.up_sample_and_concat3d(encoders_outputs[i], decoders_outputs[-1])))
         decoders_outputs.reverse()
         return decoders_outputs
 
+    def through_down_modules(self, x, decoders_outputs):
+        down_outputs, down_outputs_for_supervision = [], []
+        for i in range(self.depth):
+            down_outputs.append(F.up_sample3d(self.down[i](decoders_outputs[i]), x))
+            down_outputs_for_supervision.append(self.down_out[i](down_outputs[i]))
+        return down_outputs, down_outputs_for_supervision
+
+    def through_attention_modules(self, down_outputs):
+        attention_outputs, attention_maps = [], []
+        for i in range(self.depth):
+            mix_output, attention_map = self.attention[i](down_outputs[i])
+            attention_outputs.append(mix_output)
+            attention_maps.append(attention_map)
+        return attention_outputs, attention_maps
+
+    def through_last_block(self, mix_outputs):
+        return self.final_conv(self.non_linear(self.last_block(torch.cat(mix_outputs, dim=1))))
 
     def forward(self, x):
         x = self.non_linear(self.init_block(x))
-        encoders_outputs = self.feedforward_encoders(x)
-        decoders_outputs = self.feedforward_decoders(encoders_outputs)
-        down_outputs = [F.up_sample3d(self.down[i](decoders_outputs[i]), x) for i in range(self.num_blocks)]
-        down_outputs_for_supervision = [self.down_out[i](down_outputs[i]) for i in range(self.num_blocks)]
-        mix_outputs = []
+        encoders_outputs = self.through_encoders(x)
+        decoders_outputs = self.through_decoders(encoders_outputs)
+        down_outputs, down_outputs_for_supervision = self.through_down_modules(x, decoders_outputs)
+        mix_outputs = down_outputs
         if self.attention:
-            attention_maps = []
-            for i in range(self.num_blocks):
-                mix_output, attention_map = self.mix[i](down_outputs[i])
-                mix_outputs.append(mix_output)
-                attention_maps.append(attention_map)
-        else:
-            mix_outputs = [down_outputs[i] for i in range(self.num_blocks)]
-        mix_outputs_for_supervision = [self.mix_out[i](mix_outputs[i]) for i in range(self.num_blocks)]
-        out = self.final_conv(self.non_linear(self.last_block(torch.cat(mix_outputs, dim=1))))
+            attention_outputs, attention_maps = self.through_attention_modules(down_outputs)
+            mix_outputs = attention_outputs
+        mix_outputs_for_supervision = [self.mix_out[i](mix_outputs[i]) for i in range(self.depth)]
+        out = self.through_last_block(mix_outputs)
         outputs_for_supervision = mix_outputs_for_supervision + down_outputs_for_supervision
         return out, *outputs_for_supervision
 
